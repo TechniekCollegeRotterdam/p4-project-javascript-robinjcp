6f7b2d2fab8f6af558ff61cdbbe5b8285e853b29
Robin <robinpeereboom04@gmail.com>
2022-06-20 13:51:03 +0200

    game

---
+++ Paddle.js
@@ -0,0 +1,31 @@
+//snelheid van het bewegen van de paddles//
+const SPEED = 0.02
+
+export default class Paddle {
+  constructor(paddleElem) {
+    this.paddleElem = paddleElem
+    this.reset()
+  }
+// positie van de paddles//
+  get position() {
+    return parseFloat(
+      getComputedStyle(this.paddleElem).getPropertyValue("--position")
+    )
+  }
+
+  set position(value) {
+    this.paddleElem.style.setProperty("--position", value)
+  }
+
+  rect() {
+    return this.paddleElem.getBoundingClientRect()
+  }
+
+  reset() {
+    this.position = 50
+  }
+
+  update(delta, ballHeight) {
+    this.position += SPEED * delta * (ballHeight - this.position)
+  }
+}
